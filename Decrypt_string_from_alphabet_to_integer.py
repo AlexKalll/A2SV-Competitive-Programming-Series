40c9378644dec70b1e850afaa52e462c3ec852ee
AlexKalll <161981213+AlexKalll@users.noreply.github.com>
2024-03-22 10:53:18 +0300

    Update Decrypt_string_from_alphabet_to_integer.py

--- Decrypt_string_from_alphabet_to_integer.py
+++ Decrypt_string_from_alphabet_to_integer.py
@@ -18,3 +18,39 @@ class Solution:
                 i += 1  
                 
         return res
+
+# alos in java
+"""
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String freqAlphabets(String s) {
+        // Initialize a map to map numbers (and numbers with '#') to letters
+        Map<String, Character> m = new HashMap<>();
+        for (int i = 1; i < 10; i++) {
+            m.put(String.valueOf(i), (char) (96 + i));
+        }
+        for (int i = 10; i < 27; i++) {
+            m.put(String.valueOf(i) + '#', (char) (96 + i));
+        }
+
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        int n = s.length();
+
+        while (i < n) {
+            // If the current index is part of a 10# to 26# pattern
+            if (i + 2 < n && s.charAt(i + 2) == '#') {
+                res.append(m.get(s.substring(i, i + 3)));
+                i += 3;
+            } else {
+                res.append(m.get(String.valueOf(s.charAt(i)));
+                i++;
+            }
+        }
+
+        return res.toString();
+    }
+}
+""""

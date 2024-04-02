da6f6b3114d1598e1b07c10219a5f7627f9ebd0e
AlexKalll <161981213+AlexKalll@users.noreply.github.com>
2024-04-02 16:30:14 +0300

    Update Transpose_matrix.py
    https://leetcode.com/problems/transpose-matrix/submissions/1221142943/

--- Transpose_matrix.py
+++ Transpose_matrix.py
@@ -21,3 +21,12 @@ class Solution:
         return transpose
 
         """
+"""
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        res = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                res[col][row] = matrix[row][col]
+        return res
+"""

9ecd955be573fe9ef1c376e39ae00a41aee134b2
AlexKalll <161981213+AlexKalll@users.noreply.github.com>
2024-04-07 22:46:25 +0300

    Create Insertion_sort_part1_hackerank.py
    https://www.hackerrank.com/challenges/insertionsort1/problem

---
+++ Insertion_sort_part1_hackerank.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+
+def insertionSort1(n, arr):
+    # Write your code here
+    
+    last_num = arr[n-1]
+    if last_num < arr[0]:
+        arr[0] = last_num
+        print(*arr)
+    else:
+        for i in range(n-2, -1, -1):
+            if arr[i] > last_num:
+                arr[i+1] = arr[i]
+                print(*arr)
+            else:
+                arr[i+1] = last_num
+                print(*arr)
+                break
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)

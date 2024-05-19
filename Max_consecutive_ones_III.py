9ae56515d56c4f4c4d3379126d919ebca87cc13f
AlexKalll <161981213+AlexKalll@users.noreply.github.com>
2024-05-19 12:05:17 +0300

    Create Max_consecutive_ones_III.py
    https://leetcode.com/problems/max-consecutive-ones-iii/

---
+++ Max_consecutive_ones_III.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        max_ones = 0
+        
+        for right, num in enumerate(nums):
+            k -=(1 - num)
+
+            if k < 0:
+                k += (1 - nums[left])
+                left += 1
+            else:
+                max_ones = max(max_ones, right-left + 1)
+        return max_ones
